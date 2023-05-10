081f7655f9d6aa2d02a67b5a903ae0aae29dacc4
khrdev <kgudfhr96@gmail.com>
2023-05-10 22:32:21 +0900

    Time: 2 ms (89.09%), Space: 40.7 MB (53.35%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        HashMap<Character, Character> hs = new HashMap<>();
+        hs.put(')', '(');
+        hs.put(']', '[');
+        hs.put('}', '{');
+
+        char[] charArr = s.toCharArray();
+        
+        if (charArr.length % 2 != 0) return false;
+
+        for (Character c : charArr) {
+            if (hs.containsKey(c)) {
+                if (!stack.isEmpty() && stack.pop() == hs.get(c)) continue;
+                else return false;
+            } else {
+                stack.push(c);
+            }
+        }
+
+        if (stack.empty()) return true;
+        else return false;
+    }
+}
