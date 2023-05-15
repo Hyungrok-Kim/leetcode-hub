afda371724fb5f051d0810a28f62648bac05c7b1
khrdev <kgudfhr96@gmail.com>
2023-05-15 23:22:09 +0900

    Time: 4 ms (80.90%), Space: 52.7 MB (34.38%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -6,9 +6,8 @@ class Solution {
         int startIdx = 0, endIdx = height.length - 1, output = 0;
         while (startIdx < endIdx) {
             int xInterval = endIdx - startIdx;
-            int yInterval = height[endIdx] > height[startIdx] ? height[startIdx] : height[endIdx];
-            if (xInterval * yInterval > output)
-                output = xInterval * yInterval;
+            int yInterval = Math.min(height[endIdx], height[startIdx]);
+            output = Math.max(xInterval * yInterval, output);
 
             if (height[startIdx] <= height[endIdx])
                 startIdx++;
