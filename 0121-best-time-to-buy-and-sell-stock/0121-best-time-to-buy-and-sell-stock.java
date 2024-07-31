89f82051ad62ce2b40812ffb0057bf8314971075
hyungrok.kim <hyungrok.kim.dev@gmail.com>
2024-07-31 21:50:53 +0900

    Time: 1 ms (99.80%), Space: 61.9 MB (22.13%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int price : prices) {
+            minPrice = Math.min(minPrice, price);
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+        return maxProfit;
+    }
+}
