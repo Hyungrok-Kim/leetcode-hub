d3da23a932e6890e2debbb4c0ec2f2101f325854
hyungrok.kim <hyungrok.kim.dev@gmail.com>
2024-08-22 20:34:38 +0900

    Time: 5 ms (19.57%), Space: 42.7 MB (19.79%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -4,7 +4,6 @@ class Solution {
     public String addBinary(String a, String b) {
         BigInteger aBi = new BigInteger(a, 2);
         BigInteger bBi = new BigInteger(b, 2);
-
         return aBi.add(bBi).toString(2);
     }
 }
